6a1351756d1d1ccd46d13d41694eb608e28e0b86
tiresiah <filippodelministro@gmail.com>
2024-03-19 18:29:07 +0100

    guess_what [NOT WORKING]

--- guess_algo/dec1.c
+++ guess_algo/dec1.c
@@ -2,13 +2,20 @@
 #include <string.h>
 
 #include <openssl/evp.h>
+#include <openssl/err.h>
 
 
 #define ENCRYPT 1
+#define MAX_BUFFER 1024
 #define DECRYPT 0
 
-int main()
-{
+
+void handle_errors(){
+    ERR_print_errors_fp(stderr);
+    abort();
+}
+
+int main(int argc, char **argv){
 
 //int EVP_EncryptInit(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *type, const unsigned char *key, const unsigned char *iv);
 //int EVP_EncryptFinal(EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl);
@@ -19,52 +26,62 @@ int main()
 //  int EVP_CipherInit(EVP_CIPHER_CTX *ctx, const EVP_CIPHER *type, const unsigned char *key, const unsigned char *iv, int enc);
 //  int EVP_CipherFinal(EVP_CIPHER_CTX *ctx, unsigned char *outm, int *outl);
 
+     if(argc != 3){
+        fprintf(stderr,"Invalid parameters. Usage: %s file_in file_out\n",argv[0]);
+        exit(1);
+    }
 
+    FILE *f_in;
+    if((f_in = fopen(argv[1],"r")) == NULL) {
+            fprintf(stderr,"Couldn't open the input file, try again\n");
+            abort();
+    }
 
-
-    
+    FILE *f_out;
+    if((f_out = fopen(argv[4],"wb")) == NULL) {
+            fprintf(stderr,"Couldn't open the output file, try again\n");
+            abort();
+    }
 
     unsigned char key[] = "0123456789ABCDEF";
     unsigned char iv[]  = "0123456789ABCDEF";
-    unsigned char ciphertext_hex[] = "WlpKK0JLSk5kcFhBMmphWDhaZzVJdFJvbGExOGhpOTVNRzhmQS85UlB2Zz0K";
+
 
     EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-
-    if(!EVP_CipherInit(ctx,EVP_aes_256_cbc_hmac_sha1(), key, iv, ENCRYPT))
-        printf("Success!!\n\n");
+    EVP_CipherInit(ctx,EVP_aria_128_cbc(), key, iv, DECRYPT);
     
-    EVP_CipherInit(ctx,EVP_aes_256_cbc_hmac_sha1(), key, iv, DECRYPT);
-    
-    // convert hexstring into bytes
-    int ciphertext_len = strlen(ciphertext_hex)/2;
-    unsigned char ciphertext_binary[ciphertext_len];
-    for(int i = 0; i < ciphertext_len;i++){
-        sscanf(&ciphertext_hex[2*i],"%2hhx", &ciphertext_binary[i]);
-    }
-    for(int i = 0; i < ciphertext_len; i++)
-        printf("%02x", ciphertext_binary[i]);
-    printf("\n");
+    int length;
+    unsigned char plaintext[MAX_BUFFER+16];
 
-
-    unsigned char decrypted[ciphertext_len]; //may be larger than needed due to padding
+    int n_read;
+    unsigned char buffer[MAX_BUFFER];
 
     int update_len, final_len;
     int decrypted_len=0;
-    EVP_CipherUpdate(ctx,decrypted,&update_len,ciphertext_binary,ciphertext_len);
-    decrypted_len+=update_len;
-    printf("update size: %d\n",decrypted_len);
 
-    EVP_CipherFinal_ex(ctx,decrypted+decrypted_len,&final_len);
-    decrypted_len+=final_len;
+    while((n_read = fread(buffer,1,MAX_BUFFER,f_in)) > 0){
+        if(!EVP_CipherUpdate(ctx,plaintext,&length,buffer,n_read))
+            handle_errors();
+
+        decrypted_len+=length;
+
+        EVP_CipherFinal_ex(ctx,plaintext+decrypted_len,&final_len);
+        decrypted_len+=final_len;
+
+        if(fwrite(plaintext, 1, length,f_out) < length){
+            fprintf(stderr,"Error writing the output file\n");
+            abort();
+        }
+    }   
 
     EVP_CIPHER_CTX_free(ctx);
 
     printf("Plaintext lenght = %d\n",decrypted_len);
     for(int i = 0; i < decrypted_len; i++)
-        printf("%2x", decrypted[i]);
+        printf("%2x", plaintext[i]);
     printf("\n");
     for(int i = 0; i < decrypted_len; i++)
-        printf("%c", decrypted[i]);
+        printf("%c", plaintext[i]);
     printf("\n");
 
     return 0;
